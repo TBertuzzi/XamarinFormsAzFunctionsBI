083601e850e5980950604654dccde3ad7ab79acc
Thiago Bertuzzi <thiago.bertuzzi@gmail.com>
2020-10-05 16:13:33 -0300

    :sparkles: Implementado Formulario Inicial

--- XamarinFormsAzFunctionsBI/Models/Pessoa.cs
+++ XamarinFormsAzFunctionsBI/Models/Pessoa.cs
@@ -1,15 +1,136 @@
 ﻿using System;
+using MvvmHelpers;
+using XamarinFormsAzFunctionsBI.Validations;
+using XamarinFormsAzFunctionsBI.Validations.Rules;
+
 namespace XamarinFormsAzFunctionsBI.Models
 {
-    public class Pessoa
+    public class Pessoa : ObservableObject
     {
-        public string Email { get; set; }
-        public string DataNascimento { get; set; }
-        public string Estado { get; set; }
+        private ValidatableObject<string> _email;
+        public ValidatableObject<string> Email
+        {
+            get => _email;
+            set => SetProperty(ref _email, value);
+        }
 
-        public string Formacao { get; set; }
-        public string AreaAtuacao { get; set; }
-        public bool PossuiCertificacao { get; set; }
-        public string FaixaSalarial { get; set; }
+        private ValidatableObject<string> _dataNascimento;
+        public ValidatableObject<string> DataNascimento
+        {
+            get => _dataNascimento;
+            set => SetProperty(ref _dataNascimento, value);
+        }
+
+        private ValidatableObject<string> _estado;
+        public ValidatableObject<string> Estado
+        {
+            get => _estado;
+            set => SetProperty(ref _estado, value);
+        }
+
+        private ValidatableObject<string> _formacao;
+        public ValidatableObject<string> Formacao
+        {
+            get => _formacao;
+            set => SetProperty(ref _formacao, value);
+        }
+
+        private ValidatableObject<string> _areaAtuacao;
+        public ValidatableObject<string> AreaAtuacao
+        {
+            get => _areaAtuacao;
+            set => SetProperty(ref _areaAtuacao, value);
+        }
+
+        private ValidatableObject<string> _faixaSalarial;
+        public ValidatableObject<string> FaixaSalarial
+        {
+            get => _faixaSalarial;
+            set => SetProperty(ref _faixaSalarial, value);
+        }
+
+        private ValidatableObject<bool> _possuiCertificacao;
+        public ValidatableObject<bool> PossuiCertificacao
+        {
+            get => _possuiCertificacao;
+            set => SetProperty(ref _possuiCertificacao, value);
+        }
+
+        public Pessoa(bool validar)
+        {
+            Init();
+
+            if (validar)
+            {
+                InitValidate();
+            }
+        }
+
+        public bool Validate()
+        {
+            var email = _email.Validate();
+            var dataNascimento = _dataNascimento.Validate();
+            var estado = _estado.Validate();
+            var formacao = _formacao.Validate();
+            var areaAtuacao = _areaAtuacao.Validate();
+            var faixaSalarial = _faixaSalarial.Validate();
+
+
+            return email
+                && dataNascimento
+                && estado
+                && formacao
+                && areaAtuacao
+                && faixaSalarial;
+        }
+
+        private void Init()
+        {
+            _email = new ValidatableObject<string>();
+            _dataNascimento = new ValidatableObject<string>();
+            _estado = new ValidatableObject<string>();
+            _formacao = new ValidatableObject<string>();
+            _areaAtuacao = new ValidatableObject<string>();
+            _faixaSalarial = new ValidatableObject<string>();
+            _possuiCertificacao = new ValidatableObject<bool>();
+        }
+
+        private void InitValidate()
+        {
+            _email.Validations.Add(new IsNotNullOrEmptyRule<string>
+            {
+                ValidationMessage = "E-mail é obrigatório."
+            });
+            _email.Validations.Add(new EmailRule<string>
+            {
+                ValidationMessage = "E-mail invalido."
+            });
+
+            _estado.Validations.Add(new IsNotNullOrEmptyRule<string>
+            {
+                ValidationMessage = "O Estado é obrigatório."
+            });
+
+            _formacao.Validations.Add(new IsNotNullOrEmptyRule<string>
+            {
+                ValidationMessage = "A Formação é obrigatória."
+            });
+
+            _areaAtuacao.Validations.Add(new IsNotNullOrEmptyRule<string>
+            {
+                ValidationMessage = "A Area de Atuação é obrigatória."
+            });
+
+            _faixaSalarial.Validations.Add(new IsNotNullOrEmptyRule<string>
+            {
+                ValidationMessage = "A faixa salarial é obrigatória."
+            });
+
+            _dataNascimento.Validations.Add(new IsNotNullOrEmptyRule<string>
+            {
+                ValidationMessage = "A Data de Nascimento é obrigatória."
+            });
+
+        }
     }
 }
--- XamarinFormsAzFunctionsBI/ViewModels/MainViewModel.cs
+++ XamarinFormsAzFunctionsBI/ViewModels/MainViewModel.cs
@@ -1,18 +1,15 @@
 ﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
 using Microsoft.Extensions.Logging;
+using Xamarin.Forms;
+using XamarinFormsAzFunctionsBI.Models;
 using XamarinFormsAzFunctionsBI.ViewModels.Interfaces;
 
 namespace XamarinFormsAzFunctionsBI.ViewModels
 {
     public class MainViewModel : ViewModelBase ,IMainViewModel
     {
-        private string _hello;
-        public string Hello
-        {
-            get => _hello;
-            set => SetProperty(ref _hello, value);
-        }
-
         private int _step;
         public int Step
         {
@@ -21,17 +18,75 @@ namespace XamarinFormsAzFunctionsBI.ViewModels
             {
                 if (this.SetProperty(ref _step, value))
                 {
-                   // HabilitarPainel(_step);
+                    HabilitarPainel(_step);
                 }
             }
         }
 
+        private bool _dadosPessoais;
+        public bool DadosPessoais
+        {
+            get { return _dadosPessoais; }
+            set { SetProperty(ref _dadosPessoais, value); }
+        }
+
+        private bool _dadosProfissionais;
+        public bool DadosProfissionais
+        {
+            get { return _dadosProfissionais; }
+            set { SetProperty(ref _dadosProfissionais, value); }
+        }
+
+        private Pessoa _pessoa;
+        public Pessoa Pessoa
+        {
+            get { return _pessoa; }
+            set { SetProperty(ref _pessoa, value); }
+        }
+
+        private ICommand proximoCommand;
+        public ICommand ProximoCommand => proximoCommand ?? (proximoCommand = new Command(async () => await ProximoCommandExecute()));
+
+        private ICommand cadastrarCommand;
+        public ICommand CadastrarCommand => cadastrarCommand ?? (cadastrarCommand = new Command(async () => await CadastrarCommandExecute()));
+
         public MainViewModel(ILogger<MainViewModel> logger)
         {
-           // var httpClient = httpClientFactory.CreateClient();
-            logger.LogCritical("Always be logging!");
-            Hello = "Hello from IoC";
+            DadosPessoais = true;
+            // var httpClient = httpClientFactory.CreateClient();
+            logger.LogCritical("Acessando o Aplicativo");
         }
+
+        private async Task ProximoCommandExecute()
+        {
+       
+        }
+
+        private async Task CadastrarCommandExecute()
+        {
+            
+        }
+
+        private void HabilitarPainel(int step)
+        {
+            switch (step)
+            {
+                case 1:
+                    DadosPessoais = true;
+                    DadosProfissionais = false;
+                    break;
+                case 2:
+                    DadosPessoais = false;
+                    DadosProfissionais = true;
+                    break;
+                default:
+                    DadosPessoais = false;
+                    DadosProfissionais = false;
+                    break;
+            }
+        }
+
+       
 
     }
 }
